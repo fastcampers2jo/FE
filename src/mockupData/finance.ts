c224883e5ecc71e19411a580a118c27a098147ad
hyerimhan <hnhyerim@gmail.com>
2024-05-16 22:12:05 +0900

    finance 목업데이터 생성

---
+++ src/mockupData/finance.ts
@@ -0,0 +1,22 @@
+export const DEPOSIT = [
+  {
+    title: "우리 첫거래우대 정기예금",
+    bank: "우리은행"
+  },
+  {
+    title: "우리 첫거래우대 정기예금",
+    bank: "우리은행"
+  },
+  {
+    title: "우리 첫거래우대 정기예금",
+    bank: "우리은행"
+  }
+];
+export const INSTALLMENT = [
+  {
+    title: "우리 첫거래우대 정기적금",
+    bank: "우리은행"
+  }
+];
+export const INSURANCE = [];
+export const FUND = [];
